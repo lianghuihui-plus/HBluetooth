82ba21abac11dcecb441e3cfa2cf96935171340e
lhh <979180936@qq.com>
2020-07-28 16:43:01 +0800

    完善HBConnection.java类的读写功能

---
+++ app/src/main/java/com/lhh/hbluetooth/WriteActivity.java
@@ -0,0 +1,13 @@
+package com.lhh.hbluetooth;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WriteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_write);
+    }
+}
---
+++ hbluetooth/src/main/java/com/lhh/hbluetooth/HBReadListener.java
@@ -0,0 +1,4 @@
+package com.lhh.hbluetooth;
+
+interface HBReadListener {
+}
